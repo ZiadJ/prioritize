fcf7b97e1021e487eba8bfa5884b4507c657ee03
ZiadJ <ziadjkhan@gmail.com>
2026-04-17 10:42:12 +0400

    Prisma singleton

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,19 +11,19 @@ export { prisma };
 export default prisma;
 */
 
-const prismaClientSingleton = (): PrismaClient => {
-	const adapter = new PrismaPg({
-		connectionString: process.env.DATABASE_URL,
-	});
-	return new PrismaClient({ adapter });
+declare const global: {
+	prismaGlobal: PrismaClient;
 };
 
-declare const globalThis: {
-	prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+let prisma = global.prismaGlobal;
+if (!prisma) {
+	const adapter = new PrismaPg({
+		connectionString: process.env.DATABASE_URL
+	});
+	prisma = new PrismaClient({ adapter });
+	if (process.env.NODE_ENV !== 'production') {
+		global.prismaGlobal = prisma;
+	}
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
